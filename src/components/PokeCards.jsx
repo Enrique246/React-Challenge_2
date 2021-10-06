e2a38e1ecca8a098db621e61dea7e952cf526aa8
Enrique Del Castillo Chávez <ecch9210@gmail.com>
2021-10-05 23:26:37 -0500

    Rendered all images

--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { mockPokemonsData } from "../mock/pokeData";
 
-const pokemons = mockPokemonsData.map(function (poke) {
-  return poke.name;
-});
-
 export default function PokeCard() {
   return (
     <div>
       {mockPokemonsData.map((poke) => (
         <div>
           <h1> {poke.name} </h1>
+          <img src={poke.sprites.front_default} alt="pic" />
+          <img src={poke.sprites.front_shiny} alt="pic" />
           <img src={poke.sprites.back_default} alt="pic" />
           <img src={poke.sprites.back_shiny} alt="pic" />
         </div>
